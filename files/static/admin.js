83ea1c625430bf5c10b3c310ea242e4a37a5291f
Kierownik223 <dev@marmak.net.pl>
2025-07-03 20:22:55 +0200

    Fix system information loading on Internet Explorer 8

--- files/static/admin.js
+++ files/static/admin.js
@@ -1,34 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
+(function () {
+  function updateDisks() {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "/api/sysinfo", true);
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                try {
-                    var data = JSON.parse(xhr.responseText);
-                    var disksContainer = document.getElementById("disks");
-                    disksContainer.innerHTML = "";
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          try {
+            var data = JSON.parse(xhr.responseText);
+            var disksContainer = document.getElementById("disks");
+            disksContainer.innerHTML = "";
 
-                    for (var i = 0; i < data.disks.length; i++) {
-                        var disk = data.disks[i];
+            for (var i = 0; i < data.disks.length; i++) {
+              var disk = data.disks[i];
 
-                        var diskDiv = document.createElement("div");
-                        diskDiv.innerHTML =
-                            '<label for="usage">' + disk.used_space_readable + ' / ' + disk.total_space_readable + '</label>' +
-                            '<progress style="width:100%;box-sizing:border-box;" class="disk_usage" max="' + disk.total_space + '" value="' + disk.used_space + '"></progress>';
+              var diskDiv = document.createElement("div");
 
-                        disksContainer.appendChild(diskDiv);
-                    }
-                } catch (e) {
-                    console.error("JSON parse error:", e);
-                }
-            } else {
-                console.error("HTTP error:", xhr.status);
+              diskDiv.innerHTML = '<label for="usage">' + disk.used_space_readable + "/" + disk.total_space_readable + '</label>' + '<progress style="width:100%; box-sizing:border-box;" class="disk_usage" max="' + disk.total_space + '" value="' + disk.used_space + '"></progress>';
+
+              disksContainer.appendChild(diskDiv);
             }
+          } catch (e) {
+            if (window.console) console.error("JSON parse error:", e);
+          }
+        } else {
+          if (window.console) console.error("HTTP error:", xhr.status);
         }
+      }
     };
     xhr.onerror = function () {
+      if (window.console)
         console.error("Network error occurred during /api/sysinfo fetch.");
     };
     xhr.send();
-});
+  }
+
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
+    setTimeout(updateDisks, 0);
+  } else if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", updateDisks, false);
+  } else if (document.attachEvent) {
+    document.attachEvent("onreadystatechange", function () {
+      if (document.readyState === "complete") updateDisks();
+    });
+  }
+})();
--- files/static/sysinfo.js
+++ files/static/sysinfo.js
@@ -1,49 +1,84 @@
-document.addEventListener("DOMContentLoaded", function () {
-    function updateInfo() {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "/api/sysinfo", true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    try {
-                        var data = JSON.parse(xhr.responseText);
+(function () {
+  function updateInfo() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/api/sysinfo", true);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200 || xhr.status === 0) {
+          try {
+            var data = JSON.parse(xhr.responseText);
 
-                        document.getElementById("used_mem_readable").textContent = data.used_mem_readable;
-                        document.getElementById("total_mem_readable").textContent = data.total_mem_readable;
+            var usedMem = document.getElementById("used_mem_readable");
+            var totalMem = document.getElementById("total_mem_readable");
+            var memUsage = document.getElementById("mem_usage");
+            var disksContainer = document.getElementById("disks");
 
-                        var memUsage = document.getElementById("mem_usage");
-                        memUsage.max = data.total_mem;
-                        memUsage.value = data.used_mem;
-
-                        var disksContainer = document.getElementById("disks");
-                        disksContainer.innerHTML = "";
-
-                        for (var i = 0; i < data.disks.length; i++) {
-                            var disk = data.disks[i];
-                            var diskDiv = document.createElement("div");
-
-                            diskDiv.innerHTML =
-                                '<label for="usage">' + disk.used_space_readable + '/' + disk.total_space_readable + '</label>' +
-                                '<progress style="width:100%;box-sizing:border-box;" class="disk_usage" max="' + disk.total_space + '" value="' + disk.used_space + '"></progress>';
-
-                            disksContainer.appendChild(diskDiv);
-                        }
-                    } catch (e) {
-                        console.error("JSON parse error:", e);
-                    }
+            if (usedMem) {
+                if ('textContent' in usedMem) {
+                    usedMem.textContent = data.used_mem_readable;
                 } else {
-                    console.error("Failed to fetch system info. Status:", xhr.status);
+                    usedMem.innerText = data.used_mem_readable;
                 }
             }
-        };
 
-        xhr.onerror = function () {
-            console.error("Request failed while fetching system info.");
-        };
+            if (totalMem) {
+                if ('textContent' in totalMem) {
+                    totalMem.textContent = data.total_mem_readable;
+                } else {
+                    totalMem.innerText = data.total_mem_readable;
+                }
+            }
 
-        xhr.send();
-    }
+            if (memUsage) {
+              memUsage.max = data.total_mem;
+              memUsage.value = data.used_mem;
+            }
 
+            if (disksContainer) {
+              disksContainer.innerHTML = "";
+
+              for (var i = 0; i < data.disks.length; i++) {
+                var disk = data.disks[i];
+                var diskDiv = document.createElement("div");
+
+                diskDiv.innerHTML = '<label for="usage">' + disk.used_space_readable + "/" + disk.total_space_readable + '</label>' + '<progress style="width:100%; box-sizing:border-box;" class="disk_usage" max="' + disk.total_space + '" value="' + disk.used_space + '"></progress>';
+
+                disksContainer.appendChild(diskDiv);
+              }
+            }
+          } catch (e) {
+            if (window.console) console.error("JSON parse error:", e);
+          }
+        } else {
+          if (window.console)
+            console.error("Failed to fetch system info. Status:", xhr.status);
+        }
+      }
+    };
+
+    xhr.onerror = function () {
+      if (window.console)
+        console.error("Request failed while fetching system info.");
+    };
+
+    xhr.send();
+  }
+
+  function init() {
     updateInfo();
     setInterval(updateInfo, 2500);
-});
+  }
+
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
+    setTimeout(init, 0);
+  } else if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", init, false);
+  } else if (document.attachEvent) {
+    document.attachEvent("onreadystatechange", function () {
+      if (document.readyState === "complete") init();
+    });
+  }
+})();
