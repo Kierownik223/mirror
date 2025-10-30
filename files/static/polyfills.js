d1d19604b5b2b0f38ce4ae992d6cc94f4267ff39
Kierownik223 <marcel@marmak.net.pl>
2025-10-30 19:44:51 +0100

    Fix double slashes when using JavaScript uploader on IE11

--- files/static/polyfills.js
+++ files/static/polyfills.js
@@ -17,3 +17,12 @@ if (typeof encodeURIComponent !== "function") {
         return escape(s);
     };
 }
+
+if (!String.prototype.startsWith) {
+    Object.defineProperty(String.prototype, 'startsWith', {
+        value: function(search, rawPos) {
+            var pos = rawPos > 0 ? rawPos|0 : 0;
+            return this.substring(pos, pos + search.length) === search;
+        }
+    });
+}
