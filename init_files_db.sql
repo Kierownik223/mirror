1310fd26b13668934c09a4965980ff9616cbcb6d
Kierownik223 <marcel@marmak.net.pl>
2025-11-25 18:53:22 +0100

    Add database structure files

---
+++ init_files_db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `files` (
+    `id` uuid NOT NULL,
+    `path` text NOT NULL,
+    `downloads` int(11) NOT NULL,
+    `last_update` timestamp NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id` (`id`),
+    UNIQUE KEY `path` (`path`) USING HASH
+)
---
+++ init_marmak_db.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `logins` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`account` varchar(16) NOT NULL,
+	`time` timestamp NOT NULL,
+	`ip` varchar(255) NOT NULL,
+	`via` varchar(255) NOT NULL DEFAULT 'service',
+	PRIMARY KEY (`id`),
+	KEY `account` (`account`),
+	CONSTRAINT `account` FOREIGN KEY (`account`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE NO ACTION
+)
+
+CREATE TABLE `sessions` (
+	`id` varchar(255) NOT NULL,
+	`user` varchar(32) NOT NULL,
+	`created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+	PRIMARY KEY (`id`)
+)
+
+CREATE TABLE `users` (
+	`username` varchar(16) NOT NULL,
+	`password` varchar(255) NOT NULL,
+	`email` varchar(32) DEFAULT NULL,
+	`perms` int(2) NOT NULL DEFAULT 1,
+	`mirror_settings` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+	PRIMARY KEY (`username`),
+	UNIQUE KEY `username` (`username`)
+)
