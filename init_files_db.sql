598baf9f32cef848b5ec037ebac8d7a339b11653
Kierownik223 <marcel@marmak.net.pl>
2026-02-01 16:42:09 +0100

    Release 1.7.0

--- init_files_db.sql
+++ init_files_db.sql
@@ -3,6 +3,7 @@ CREATE TABLE `files` (
 	`path` text NOT NULL,
 	`downloads` int(11) NOT NULL,
 	`last_update` timestamp NOT NULL DEFAULT current_timestamp(),
+	`shared` bool NOT NULL default 0,
 	PRIMARY KEY (`id`),
 	UNIQUE KEY `id` (`id`),
 	UNIQUE KEY `path` (`path`) USING HASH
