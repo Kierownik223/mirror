9152f6d82b860ecee5f524ad214d5b4e2cef97d0
Kierownik223 <marcel@marmak.net.pl>
2026-02-01 15:27:13 +0100

    Deduplicate XHR boilerplate

---
+++ public/static/utils.js
@@ -0,0 +1,40 @@
+function sendRequest(options) {
+    var xhr;
+
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else {
+        try {
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (e) {
+            if (options.onInitError) options.onInitError(e);
+            return null;
+        }
+    }
+
+    try {
+        xhr.open(options.method, options.url, true);
+        if (options.contentType) xhr.setRequestHeader("Content-Type", options.contentType);
+    } catch (e) {
+        if (options.onOpenError) options.onOpenError(e);
+        return null;
+    }
+
+    xhr.onreadystatechange = function () {
+        if (options.onReady) {
+            options.onReady(xhr);
+        }
+    };
+
+    try {
+        if (options.body !== undefined) {
+            xhr.send(options.body);
+        } else {
+            xhr.send();
+        }
+    } catch (e) {
+        if (options.onSendError) options.onSendError(e);
+    }
+
+    return xhr;
+}
